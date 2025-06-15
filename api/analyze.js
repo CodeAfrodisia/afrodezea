2ad353afb9e76d49650ccc326a310601ba83db6c
Kristopher Robinson <kristopherrobinson@KristophersiMac.attlocal.net>
2025-06-14 22:18:19 -0700

    Use env vars for Supabase config

--- api/analyze.js
+++ api/analyze.js
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js"
 const sentiment = new Sentiment()
 
 const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_URL,
     process.env.SUPABASE_ANON_KEY
 )
 
